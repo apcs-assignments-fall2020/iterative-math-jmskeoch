ba88edae36318060330fd2e1e24a0c01560fb619
jmskeoch <jmskeoch@yahoo.com>
2020-09-14 01:56:24 +0000

    Add three iterative math methods w/ user input
    Square root (babylonian method)
    Factorial (factorial method)
    E calculator (calculateE method)

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,32 +4,56 @@ public class MyMain {
     
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double n = Math.round(Math.random() * 100 + 1);
+        double m = (n + x/n) / 2;
+
+        while(Math.abs(n - m) > 0.00001) {
+            n = m;
+            m = (n + x/n) / 2;
+        }
+
+        return n;
+            
     }
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double result = 1;
+        if(x == 0) {
+            return 1;
+        }
+        
+        for(int i = x; i > 0; i--) {
+            result = result * i;
+        }
+        return result;
     }
 
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double e = 0.0;
+        int i = 0;
+        
+        while(Math.abs(e - Math.E) > 0.00001) {
+            e = e + 1/factorial(i);
+            i++;
+        }
+
+        return e;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // YOUR CODE HERE
+        System.out.println("Enter a positive number!: ");
+        double input = scan.nextDouble();
 
+        System.out.println("The square root of " + input + " = " + babylonian(input));
+        System.out.println("E is roughly " + calculateE());
 
-        
 
         scan.close();
     }
